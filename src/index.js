b4fbb616bec9a9c79ec6ea5b22aa55e9b5d55d61
Svyatoslav <svyatoslav.orlov11@gmail.com>
2022-09-15 00:16:17 +0300

    refactoring complete

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import "./index.css"
-import reportWebVitals from "./reportWebVitals"
-import User, { randerPhrase } from "./components/user"
-import "bootstrap/dist/css/bootstrap.css"
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import reportWebVitals from './reportWebVitals'
+import User from './components/user'
+import 'bootstrap/dist/css/bootstrap.css'
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <User />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<User />
+	</React.StrictMode>,
 )
 
 reportWebVitals()
